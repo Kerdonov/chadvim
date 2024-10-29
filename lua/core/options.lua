45a81c7bc64d547f7733ef1550a2ee9810b04861
Kerdonov <jalukse.kert@gmail.com>
2024-10-30 00:12:20 +0200

    most basic options

---
+++ lua/core/options.lua
@@ -0,0 +1,10 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.cursorline = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.wo.relativenumber = true
